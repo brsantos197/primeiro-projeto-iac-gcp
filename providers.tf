453b4930f62ed28e5703c98bddfda4bc099aaa84
Bruno Santos <brsantos197@gmail.com>
2024-06-16 23:08:06 -0300

    first commit

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.33.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "rocketseat-iac"
+  region  = "us-central1"
+}
